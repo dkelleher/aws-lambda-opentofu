1a83ac6108b34bc0ad375fa8df423078372ca11e
Dan Kelleher <dk.kelleher@gmail.com>
2024-12-05 23:54:41 -0500

    Add main OpenTofu configuration

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    # Configure these values in backend-config.tfvars
+    key = "lambda/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/src/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+resource "aws_lambda_function" "function" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = var.project_name
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "python3.11"
+
+  environment {
+    variables = var.environment_variables
+  }
+}
